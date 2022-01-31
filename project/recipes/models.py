1445d9295c16c82d72d3874a9eacf7290584cb4c
adriaizpu-travelperk <adrian.aizpurua@travelperk.com>
2022-01-31 14:17:02 +0100

    increased description field length to 3k chars

--- project/recipes/models.py
+++ project/recipes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Recipe(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=3000)
 
     def __str__(self):
         return self.name
